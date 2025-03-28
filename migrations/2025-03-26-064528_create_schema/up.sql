5dee29477119bd334cf6817ccaccce117c2cd3fe
Gerd Reiss <gerd@reiss.pro>
2025-03-28 07:48:34 +0100

    fix sql

--- migrations/2025-03-26-064528_create_schema/up.sql
+++ migrations/2025-03-26-064528_create_schema/up.sql
@@ -2,5 +2,5 @@ CREATE TABLE phrases (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     original TEXT NOT NULL UNIQUE,
     pinyin TEXT,
-    translation TEXT NOT NULL,
+    translation TEXT NOT NULL
 );
