dafdea44a32a1c1a471ce6e5f894b7b0afb157b6
Gerd Reiss <gerd@reiss.pro>
2025-06-18 09:50:31 +0200

    minor change

--- src/persistence/database_connection.rs
+++ src/persistence/database_connection.rs
@@ -1,6 +1,6 @@
 use diesel::prelude::*;
 
-pub(crate) fn create(database_url: &str) -> Result<diesel::SqliteConnection, super::PersistenceError> {
-    let connection = diesel::SqliteConnection::establish(database_url)?;
+pub(crate) fn create(database_url: &str) -> Result<SqliteConnection, super::PersistenceError> {
+    let connection = SqliteConnection::establish(database_url)?;
     Ok(connection)
 }
